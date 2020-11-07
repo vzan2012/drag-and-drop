98e7ef37544c87b56a6e4edf897baf7c84bc1e4f
Deepak Guptha <vzan2012@gmail.com>
2020-11-07 22:16:46 +0530

    feat: Updation of User Inputs

--- src/app.ts
+++ src/app.ts
@@ -38,13 +38,38 @@ class ProjectInput {
         this.attach()
     }
 
+    private gatherUserInput(): [string, string, number] | void {  // Type : Tuple | void
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = (this.peopleInputElement.value);
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert(`Invalid Entry. Please try again !!!`)
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    // Clear the fields 
+    private clearFields() {
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.peopleInputElement.value = ''
+    }
+
     // Event Handler for the form
     @autobind
     private submitHandler(e: Event) {
         e.preventDefault();
-        console.log(this.titleInputElement.value)
-        console.log(this.descriptionInputElement.value)
-        console.log(this.peopleInputElement.value)
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput
+            console.log(title, desc, people)
+            this.clearFields()
+        }
+        // console.log(this.titleInputElement.value)
+        // console.log(this.descriptionInputElement.value)
+        // console.log(this.peopleInputElement.value)
     }
 
     private configure() {
