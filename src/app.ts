891e936412d3d58fc8a31700c0d920db25851b45
Deepak Guptha <vzan2012@gmail.com>
2020-11-11 18:13:26 +0530

    feat: Render project lists

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,35 @@ function autobind(target: any, methodName: string, descriptor: PropertyDescripto
     return adjDescriptor
 }
 
+// Project List Class 
+class ProjectList {
+    templateElement: HTMLTemplateElement // For the id - project-input - section
+    hostElement: HTMLDivElement // For the id - app - section
+    element: HTMLElement
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement
+        this.hostElement = document.getElementById("app")! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLElement
+
+        this.element.id = `${this.type}-projects`
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId
+        this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
+    }
+}
+
 
 // Project Input Class
 class ProjectInput {
@@ -136,3 +165,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
