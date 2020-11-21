65163a8b2d7cce7cbed5e09a2a1ce36102de1c5d
Deepak Guptha <vzan2012@gmail.com>
2020-11-21 21:17:00 +0530

    feat: Updating the Drag Events

--- src/app.ts
+++ src/app.ts
@@ -179,7 +179,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 }
 
 // Project List Class 
-class ProjectList extends Component<HTMLDivElement, HTMLElement>{
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
     assignedProjects: Project[] = []
     constructor(private type: 'active' | 'finished') {
         super('project-list', 'app', false, `${type}-projects`);
@@ -189,7 +189,25 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         this.renderContent();
     }
 
+    @autobind
+    dragOverHandler(event: DragEvent) {
+        const listEl = this.element.querySelector('ul')!
+        listEl.classList.add('droppable')
+    }
+
+    dragHandler(event: DragEvent) { }
+
+    @autobind
+    dragLeaveHandler(event: DragEvent) {
+        const listEl = this.element.querySelector('ul')!
+        listEl.classList.remove('droppable')
+    }
+
     configure() {
+        this.element.addEventListener('dragover', this.dragOverHandler)
+        this.element.addEventListener('dragleave', this.dragLeaveHandler)
+        this.element.addEventListener('drop', this.dragLeaveHandler)
+
         projectState.addListener((projects: Project[]) => {
             const relevantProjects = projects.filter(prj => {
                 if (this.type === 'active') {
