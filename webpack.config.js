ef999fe57015daeb3023b8735ce31c84f5d1a3c5
Deepak Guptha <vzan2012@gmail.com>
2020-12-16 18:34:41 +0530

    chore: updating with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
+  devServer: {
+    publicPath: "/dist/",
+  },
   module: {
     rules: [
       {
